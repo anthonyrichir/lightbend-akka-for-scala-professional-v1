f8290a594f55f21624e4fd427637b60a25688a89
Anthony Richir <arichir@altissia.com>
2020-04-22 21:44:46 +0200

    Message an Actor

--- exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
@@ -1,8 +1,14 @@
 package com.lightbend.training.coffeehouse
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
+
+object CoffeeHouse {
+  def props(): Props = Props(new CoffeeHouse)
+}
 
 class CoffeeHouse extends Actor with ActorLogging {
+
+  log.debug("CoffeeHouse Open")
 
   override def receive: Receive = {
     case _ => log.info("Coffee Brewing")
--- exercises/src/test/resources/README.md
+++ exercises/src/test/resources/README.md
@@ -1,3 +1,3 @@
-Implement an Actor
+Message an Actor
 
 Please refer to the instructions in the Lightbend Academy.
